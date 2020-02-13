f3dde8324dd64c9d761dd90e8da8cdb1fa0fe8ea
Tony Lee <ssbmaster@hotmail.com>
2020-02-12 20:46:35 -0500

    Add files via upload
    Added the ability to save the model to ".rda" file.
    Todo: clean up the variable names.

--- precinctprediction.R
+++ precinctprediction.R
@@ -20,7 +20,7 @@ fit <- rpart(did_crash_happen ~ ., train_data, method="anova")
 fit$cptable[which.min(fit$cptable[,"xerror"]),"CP"]
 prune(fit, cp=0.01)
 predicted_val <- predict(fit, test_data)
-probability <- predict(fit, test_data, type = 'prob')
+#probability <- predict(fit, test_data, type = 'prob')
 
 splitfun <- function(x, labs, digits, varlen, faclen)
 {
@@ -33,9 +33,13 @@ splitfun <- function(x, labs, digits, varlen, faclen)
   labs
 }
 
+# Generate the plots
 rpart.plot(fit, main="Regression Tree", box.palette = "Blues", under=TRUE, roundint = FALSE, clip.right.labs = FALSE, type=3, split.fun=splitfun)
 rpart.rules(fit, cover=TRUE, roundint = FALSE)
 auc.rp = roc(test_data$did_crash_happen, factor(predicted_val, ordered = TRUE), plot = TRUE, col = "blue")
-post(fit, file = "tree.ps", title = "Pruned Regression Tree for Crashes")
-#auc(test_data$did_crash_happen, predicted_val)
+#post(fit, file = "tree.ps", title = "Pruned Regression Tree for Crashes")
+auc(test_data$did_crash_happen, predicted_val)
 auc.rp
+
+# Save the model into a file
+save(fit, file="fittedRegTreeModel.rda")
