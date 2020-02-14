08fba39c6642b4502cee4ddc5b4527fab5fa5588
Tony Lee <ssbmaster@hotmail.com>
2020-02-13 23:44:08 -0500

    Add files via upload
    Added most of the elements of the UI, beginning skeleton of serverside code.  Still need to figure out how best to update and load the map.  Still need to write code to do the prediction with the loaded model.

--- app.R
+++ app.R
@@ -9,26 +9,62 @@
 
 library(shiny)
 
+# Load the model here, once per R session; most efficient outside of server function
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("NYC Crash Predictor"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            sliderInput("crashmonth",
+                        "Pick a month:",
+                        min = 1,
+                        max = 12,
+                        value = 6,
+                        step = 1),
+            radioButtons("crashdayofweekorday",
+                         "Predict by day of the week or a specific day:",
+                         c("Day of the week" = "dayofweek",
+                           "Specific day" = "day")),
+            selectInput("crashdayofweek",
+                        "Pick a day of the week:",
+                        c("Monday" = 1,
+                          "Tuesday" = 2,
+                          "Wednesday" = 3,
+                          "Thursday" = 4,
+                          "Friday" =  5,
+                          "Saturday" = 6,
+                          "Sunday" = 7)),
+            sliderInput("crashday",
+                        "Pick a day:",
+                        min = 1,
+                        max = 31,
+                        value = 15),
             sliderInput("crashhour",
-                        "Time of day:",
+                        "Pick a time:",
                         min = 0,
                         max = 24,
                         value = 12,
-                        step = 0.5)
+                        step = 0.5),
+            actionButton("predictbutton",
+                         "Predict!")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            
+            # Can use tags$xxx() to represent xxx html tags, HTML("html stuff") to interpret HTML
+            tags$p("Check out our:",
+                   tags$a(href = "https://github.com/patrick-osborne/CSML1000-Group-10-assignment-1", "Github")),
+            tags$img(height = 300,
+                     width = 400,
+                     src = "xxx.png"),
+            plotOutput("map")
         )
     )
 )
@@ -36,14 +72,14 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    crashmonth <- reactive({input$crashmonth})
+    crashday <- isolate({input$crashday})
+    
+    # run code with eventReactive() or observeEvent()? hmmmm.
+    observeEvent(input$predictbutton, {print(input$crashmonth)}) #, input$crashday, input$crashdayofweek, input$crashdayofweekorday, input$crashhour)})
+    
+    output$map <- renderPlot({print(crashmonth())})
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
